9a3f199e943f9502b28be5dbed3922282cccd600
ayws <32442528+ayws@users.noreply.github.com>
2018-04-11 09:05:16 -0400

    Update MyTools.java

--- src/student_player/MyTools.java
+++ src/student_player/MyTools.java
@@ -8,6 +8,8 @@ import java.util.List;
 import java.util.Random;
 
 import boardgame.Board;
+import boardgame.BoardState;
+import boardgame.Move;
 import coordinates.Coord;
 import coordinates.Coordinates;
 import tablut.TablutBoardState;
@@ -15,181 +17,142 @@ import tablut.TablutMove;
 
 public class MyTools {
 	
-//	https://github.com/rameshvarun/Hnefatafl/blob/master/app/src/main/java/net/varunramesh/hnefatafl/ai/MinimaxStrategy.java
-    
+	public static int player;
+	public static int opponent;
+	public static boolean first_play = true;
+    private static Random rand = new Random(1842);
+ 	public static int MAX_TIME = 1990; // time limit is 2 seconds - buffer
+ 	public static int FIRST_MAX_TIME = 29990;
 	
- 	private static boolean outOfTime = false;
- 	public static HashMap<TablutBoardState, Integer> stateCache = new HashMap<>();
+	// returns the number of simulations we can run, based on whether it's our first move or not
+	public static int getNumSimulations() {
+		if (first_play) {
+			first_play = false;
+			return 120;
+		} else {
+			return 8;
+		}
+	}
 	
-	public static double getSomething() {
-        return Math.random();
-    }
-    
-    // time limited iterative depth first search, using alpha-beta pruning.
-    public static int TimeLimitedIDS(TablutBoardState boardState, int player_id, long timeLimit) {
-    
-     	int depth = 1;
-     	int best = Integer.MIN_VALUE;
-     	long start = System.currentTimeMillis();
-     	long end = start + timeLimit;
-     	outOfTime = false;
-     	
-     	// if we already know the score for a certain board arrangement, just return its cached score
-     	if(stateCache.containsKey(boardState)) {
-     		return stateCache.get(boardState);
-     	}
-     	
-     	while(true) {
-     		long current = System.currentTimeMillis();
-     		
-     		if (end <= current) {
-     			break;
-     		}
-     		int alpha = Integer.MIN_VALUE;
-     		int beta = Integer.MAX_VALUE;
-     		int moves_to_date = 1;
-     		
-     		int res = AlphaBeta(boardState, depth, moves_to_date, start, end, alpha, beta, player_id);
-     		stateCache.put(boardState, res);
-     		
-     		
-     		if (res > 500) {
-     			return res;
-     		}
-     		
-     		if(!outOfTime) {
-     			best = res;
-     		}
-     		
-     		depth++;
-     	}
-     	return best;	
-    }
-    
-        
-    public static int AlphaBeta(TablutBoardState bs, int depth, int num_moves, long start, long end, int alpha, int beta, int player_id) {
-    		
-  		ArrayList<TablutMove> legal_moves = bs.getAllLegalMoves();
-  		boolean ourPlay = bs.getTurnNumber() == player_id;
-  		
-  		int score = scoreState(bs, player_id, num_moves);
-  		
-  		if((System.currentTimeMillis() - start) >= end) {
-  			outOfTime = true;
-  		}
-  		
-  		if(depth == 0 || legal_moves.size()==0 || 500 <= score || score <= -500 ) {
-  			return score;
-  		}
-  		
-  		if(ourPlay) {
-  			for(TablutMove m: legal_moves) {
-  				TablutBoardState child = (TablutBoardState) bs.clone();
-  				child.processMove(m);
-  				int newSearch = AlphaBeta(child, depth-1, num_moves+1, start, end, alpha, beta, player_id);
-  				alpha = Math.max(newSearch, alpha);
-  				if (alpha >= beta) {
-  					break;
-  				}
-  			}
-  			return alpha;
-  		} else {
-  			for(TablutMove m: legal_moves) {
-  				TablutBoardState child = (TablutBoardState) bs.clone();
-  				child.processMove(m);
-  				int newSearch = AlphaBeta(child, depth-1, num_moves+1, start, end, alpha, beta, player_id);
-  				beta = Math.min(newSearch, beta);
-  				if (alpha >= beta) {
-  					break;
-  				}
-  			}
-  			return beta;
-  		}
-  		
-    		
-    }
-    
+	public static TablutMove MonteCarlo(TablutBoardState bs, int player_id) {
+		
+		player = player_id;
+		opponent = (player_id == TablutBoardState.SWEDE) ? TablutBoardState.MUSCOVITE : TablutBoardState.SWEDE;
+		long timeLimit;
+		
+		List<TablutMove> legal = bs.getAllLegalMoves();
+		int best_win_rate = 0;
+		TablutMove best_move = (TablutMove) bs.getRandomMove();
+		
+		// set a time limit
+		if(first_play) {
+			timeLimit = System.currentTimeMillis() + FIRST_MAX_TIME;
+		} else {
+			timeLimit = System.currentTimeMillis() + MAX_TIME;
+		}
+		
+		for(TablutMove m: legal) {
+			
+			int win_rate = 0;
+			
+			for(int i = 0; i < getNumSimulations(); i++) { // run a number of simulations per move
+				
+				// check if we are timed out
+				if(System.currentTimeMillis() >= timeLimit) {
+					return best_move;
+				}
+				
+				TablutBoardState clone = (TablutBoardState) bs.clone();
+				int score = simulateGame(clone, m);
+				if(score == Integer.MAX_VALUE) {
+					win_rate++;
+				}
+			}
+			if(win_rate > best_win_rate) {
+				best_win_rate = win_rate;
+				best_move = m;
+			}
+			
+		}
+		
+		return best_move;
+	}
+	
+	// simulates the game until the end
+	// instead of making random moves as per regular Monte Carlo simulation, we use a greedy move
+	public static int simulateGame(TablutBoardState bs, TablutMove m) {
+		
+		bs.processMove(m);
+		while(bs.getWinner() == Board.NOBODY) {
+			if(Math.random() < 0.05) {
+				TablutMove move = (TablutMove) bs.getRandomMove();
+				bs.processMove(move);
+			} else {
+				TablutMove move = (TablutMove) chooseGreedyMove(bs, bs.getTurnPlayer());
+				bs.processMove(move);
+			}
+		}
+		
+		if(bs.getWinner() == player) {
+			return Integer.MAX_VALUE;
+		} else if (bs.getWinner() == opponent) {
+			return Integer.MIN_VALUE;
+		} else {
+			return 0;
+		}
+	}
+	
+	// Greedy function used in the game simulation.
+	// Chosen because it performed better than simulating with random moves.
+	public static Move chooseGreedyMove(TablutBoardState bs, int player_id) {
+	    List<TablutMove> options = bs.getAllLegalMoves();
 
-    public static int scoreState(TablutBoardState bs, int player_id, int num_moves) {
-    		
-    		int opponent = bs.getOpponent();
-	    	if(bs.getWinner() == Board.DRAW) {
-	    		return 0;
-	    } 
-	    // check if in the board state, our last move caused a win
-	    // prioritize wins that take a shorter number of total moves to reach
-	    else if (bs.getWinner() == player_id) {
-	    		return 1000 - num_moves;
-	    	// otherwise check if it lead to our opponent winning
-	    } else if (bs.getWinner() == opponent) { 
-	    		return  -1000;
+
+	    TablutMove bestMove = options.get(rand.nextInt(options.size()));
+
+	    int opponent = bs.getOpponent();
+	    int minNumberOfOpponentPieces = bs.getNumberPlayerPieces(opponent);
+	    boolean moveCaptures = false;
+
+	    for (TablutMove move : options) {
+
+	        TablutBoardState cloneBS = (TablutBoardState) bs.clone();
+
+	        cloneBS.processMove(move);
+
+	        int newNumberOfOpponentPieces = cloneBS.getNumberPlayerPieces(opponent);
+
+	        if (newNumberOfOpponentPieces < minNumberOfOpponentPieces) {
+	            bestMove = move;
+	            minNumberOfOpponentPieces = newNumberOfOpponentPieces;
+	            moveCaptures = true;
+	        }
+
+	        if (cloneBS.getWinner() == player_id) {
+	            bestMove = move;
+	            moveCaptures = true;
+	            break;
+	        }
 	    }
-    	
-    		int score = 0;
-    		int numOfOpponentPieces = bs.getNumberPlayerPieces(opponent);
-    		int numOfOurPieces = bs.getNumberPlayerPieces(player_id);
 
+	 
+	    if (player_id == TablutBoardState.SWEDE && !moveCaptures) {
+	        Coord kingPos = bs.getKingPosition();
 
-      //get pieces surrounding the king
-       // if king is not in the middle, weigh surrounding opponent neighbours more
-        // since we only need two to sandwich
-        Coord kingPos = bs.getKingPosition();
-		int distToCorner = Coordinates.distanceToClosestCorner(kingPos);
-        List<Coord> kingNeighbours = Coordinates.getNeighbors(kingPos);
-        int count_n = 0;
-        if(Coordinates.isCenterOrNeighborCenter(kingPos)) {
-	        	
-        } 
-        for(Coord n: kingNeighbours) { 
-     		if(bs.isOpponentPieceAt(n) || Coordinates.isCenter(n) || Coordinates.isCorner(n)) {
-     			if(Coordinates.isCenterOrNeighborCenter(kingPos)) {
-     				count_n++;
-     			} else {
-     				count_n+=2;
-     			}
-     		}
-    	 	}
-        
-        Coord KingsClosestCorner = getClosestCorner(kingPos);
-        List<Coord> kingToCornerCoords = kingPos.getCoordsBetween(KingsClosestCorner);
-        int intercepting = 0;
-        for(Coord c: kingToCornerCoords) {
-        		if(bs.isOpponentPieceAt(c)) {
-        			intercepting++;
-        		}
-        }
+	        int minDistance = Coordinates.distanceToClosestCorner(kingPos);
 
-        
-        //if we are swedes, a larger distance between king and corner is bad
-        if (player_id == TablutBoardState.SWEDE) {
-        		score -= distToCorner/2;
-        		score -= count_n;
-        		score -= intercepting;
-        } else {
-        		score += distToCorner/2;
-        		score += count_n;
-        		score += intercepting;
-        }
-        
-        
-        
-    		return score;
-    }
-    
-    
- // Given a coordinate, returns the closest corner.
-    public static Coord getClosestCorner(Coord pos) {
-        List<Coord> corners = Coordinates.getCorners();
-        int minDistance = Integer.MAX_VALUE;
-        Coord closestCorner = null;
-        for (Coord corner : corners) {
-            int distance = pos.distance(corner);
-            if (distance < minDistance) {
-                minDistance = distance;
-                closestCorner = corner;
-            }
-        }
-        return closestCorner;
-    }
-
+	        for (TablutMove move : bs.getLegalMovesForPosition(kingPos)) {
+	            int moveDistance = Coordinates.distanceToClosestCorner(move.getEndPosition());
+	            if (moveDistance < minDistance) {
+	                minDistance = moveDistance;
+	                bestMove = move;
+	            }
+	        }
+	    }
+	    return bestMove;
+	}
+	
 }
+
+
+
